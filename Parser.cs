efc26e540ba5c9904a0d146dcfd3862bc9859fb1
Rocky Mishra <Rkymishra49@outlook.com>
2022-05-16 21:17:48 +0545

    project imported

---
+++ Parser.cs
@@ -0,0 +1,36 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayorElectionWebScraping_Kathmandu_Kathmandu
+{
+    public static class Parser
+    {
+        public static List<Tuple<string, string>> ParseHtml(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            List<HtmlNode>? candidateNameNodes = htmlDoc.DocumentNode.Descendants("div")
+                    .Where(node => node.GetAttributeValue("class", "candidate-name").Contains("candidate-name")).ToList();
+
+            List<HtmlNode>? candidateVoteCountNodes = htmlDoc.DocumentNode.Descendants("div")
+                    .Where(node => node.GetAttributeValue("class", "vote-numbers").Contains("vote-numbers")).ToList();
+
+            List<Tuple<string, string>> nameVoteList = new List<Tuple<string, string>>();
+            var nameAndVotes = candidateNameNodes.Zip(candidateVoteCountNodes, (n, v) => new { Name = n.InnerHtml.Trim(), Votes = v.InnerHtml.Trim() });
+
+            foreach (var item in nameAndVotes)
+            {
+                if (!string.IsNullOrEmpty(item.Name) && !item.Name.Contains("<"))
+                {
+                    string voteCount = string.IsNullOrEmpty(item.Votes) ? "Not Counted" : item.Votes;
+                    nameVoteList.Add(new Tuple<string, string>(item.Name, voteCount));
+                }
+            }
+            return nameVoteList;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HtmlAgilityPack;
+using MayorElectionWebScraping_Kathmandu_Kathmandu;
+using System.Net;
+using System.Text;
+
+try
+{
+    string electionUrl = "https://election.ekantipur.com/pradesh-3/district-kathmandu/kathmandu?lng=eng";
+    string htmlFileLocation = @"D:\Private\.NET Projects\MayorElectionWebScraping_Kathmandu_Kathmandu\Htmls\index.html";
+    string csvFileLocation = @"D:\Private\.NET Projects\MayorElectionWebScraping_Kathmandu_Kathmandu\Htmls\nameVoteList.csv";
+    HttpClient client = new HttpClient();
+    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+    client.DefaultRequestHeaders.Accept.Clear();
+    string response = await client.GetStringAsync(electionUrl);
+    using (var stream = File.Open(htmlFileLocation, FileMode.OpenOrCreate))
+    {
+        stream.Write(Encoding.UTF8.GetBytes(response));
+    }
+    var nameVoteList = Parser.ParseHtml(response);
+    using (var stream = File.Open(csvFileLocation, FileMode.OpenOrCreate))
+    {
+        foreach (var item in nameVoteList)
+        {
+            stream.Write(Encoding.UTF8.GetBytes($"\n{item.Item1}, {item.Item2}"));
+        }
+    }
+    Console.WriteLine($"{nameVoteList.Count()} data found!");
+}
+catch (Exception ex)
+{
+    throw;
+}
